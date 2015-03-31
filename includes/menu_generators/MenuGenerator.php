df09967b08297a145a4399e7b06092e4ba04cb10
Matthew Grasmick <matthew.grasmick@acquia.com>
2015-03-31 16:41:15 -0400

    JUS-2720 Adding custom menu generator for UST.

--- includes/menu_generators/MenuGenerator.php
+++ includes/menu_generators/MenuGenerator.php
@@ -72,6 +72,9 @@ class MenuGenerationParameters {
   }
 }
 
+/**
+ * Class MenuGenerator.
+ */
 class MenuGenerator {
   private $parameters;
   private $engine;
@@ -111,6 +114,9 @@ class MenuGenerator {
   }
 }
 
+/**
+ * Class MenuGeneratorEngineDefault.
+ */
 class MenuGeneratorEngineDefault {
   protected $parameters;
 
@@ -133,6 +139,9 @@ class MenuGeneratorEngineDefault {
 
   /**
    * Get a qp() object.
+   *
+   * @return QueryPath
+   *   The query path object.
    */
   protected function getQueryPath() {
     if (!$this->queryPath) {
@@ -211,7 +220,13 @@ class MenuGeneratorEngineDefault {
     // @todo This is cheating, this will work with districts, but not
     // generally.
     $base = $this->parameters->getJusticeUrl();
-    $subpath = str_replace($base, '', $this->parameters->getUriMenuLocation());
+
+    if ($local_base_uri = $this->parameters->getUriLocalBase()) {
+      $subpath = str_replace($base, '', $local_base_uri);
+    }
+    else {
+      $subpath = str_replace($base, '', $this->parameters->getUriMenuLocation());
+    }
 
     module_load_include('inc', 'doj_migration', 'includes/doj_migration');
     $legacy_uri = doj_migration_relative_to_absolute_url($uri, $base, $subpath);
@@ -248,50 +263,5 @@ class MenuGeneratorEngineDefault {
   public function generate() {
     $this->recurse();
     return $this->content;
-  }
-}
-
-class MenuGeneratorEngineDistrict extends MenuGeneratorEngineDefault {
-  /**
-   * {@inheritdoc}
-   */
-  protected function recurse($css_selector = NULL, $prefix = "") {
-    module_load_include("inc", "doj_migration", "includes/doj_migration");
-    if (!isset($css_selector)) {
-      $css_selector = $this->initialCssSelector;
-    }
-    $pre = $prefix;
-    $css_selector .= ">li";
-
-    drush_doj_migration_debug_output("CSS INITIAL: $css_selector \n");
-    drush_doj_migration_debug_output("PRE: $pre \n");
-
-    $query = $this->getQueryPath();
-
-    // In the districts, inside the ul all we have are lis.
-    $elements = $query->find($css_selector . ">*");
-
-    // And inside the lis, we might have other uls to recurse on,
-    // or anchor elements, lets deal with both those cases.
-    foreach ($elements as $elem) {
-      if ($elem->is("ul")) {
-        drush_doj_migration_debug_output('Im in a ul');
-        $class_name = doj_migration_random_string();
-        $elem->attr('class', $class_name);
-        $this->recurse("{$css_selector}>ul.{$class_name}", "{$pre}-");
-      }
-      elseif ($elem->is("a")) {
-        drush_doj_migration_debug_output('Im in a anchor');
-        $a = $elem;
-
-        $al = $a->text();
-        $uri = $this->normalizeUri($a->attr("href"));
-        $line = "{$pre}{$al}{\"url\":\"{$uri}\"}\n";
-        drush_doj_migration_debug_output("CSS INNER $al: $css_selector \n");
-        drush_doj_migration_debug_output("PRE INNER: $pre \n");
-        drush_doj_migration_debug_output("LINE: $line");
-        $this->content .= $line;
-      }
-    }
   }
 }
