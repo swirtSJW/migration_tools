c5bbf48f3a2fc79e74b8a4dd0d44397ebf5d40a8
Matthew Grasmick <matthew.grasmick@acquia.com>
2015-06-10 23:56:51 -0400

    JUS-2652 Updating ATR Speech migration to fix file attachment paths.

--- includes/source_parsers/NGNodeSourceParser.php
+++ includes/source_parsers/NGNodeSourceParser.php
@@ -48,25 +48,6 @@ class NGNodeSourceParser extends NGSourceParser {
   }
 
   /**
-   * Set the html var after some cleaning.
-   *
-   * @todo this is specific to justice so it should not be here.
-   */
-  protected function cleanHtml() {
-    try {
-      $this->initQueryPath();
-      HtmlCleanUp::convertRelativeSrcsToAbsolute($this->queryPath, $this->fileId);
-      HtmlCleanUp::removeFaultyImgLongdesc($this->queryPath);
-
-      // Clean up specific to the Justice site.
-      HtmlCleanUp::stripOrFixLegacyElements($this->queryPath);
-    }
-    catch (Exception $e) {
-      $this->sourceParserMessage('@file_id Failed to clean the html, Exception: @error_message', array('@file_id' => $this->fileId, '@error_message' => $e->getMessage()), WATCHDOG_ERROR);
-    }
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function setDefaultObtainersInfo() {
