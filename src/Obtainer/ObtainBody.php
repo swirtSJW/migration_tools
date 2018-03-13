04496d173b4dc80547360292ee3e6e915d5b2a52
Kristian Ducharme <kducharm@3072643.no-reply.drupal.org>
2018-03-13 13:49:08 -0700

    Added cleanString to ObtainBody.

--- src/Obtainer/ObtainBody.php
+++ src/Obtainer/ObtainBody.php
@@ -27,4 +27,23 @@ class ObtainBody extends ObtainHtml {
     $element = $this->queryPath->top()->find('body');
     return $element->innerHtml();
   }
+
+  /**
+   * Cleans $text and returns it prior to validation.
+   *
+   * @param string $string
+   *   Text to clean and return.
+   *
+   * @return string
+   *   The cleaned text.
+   */
+  public static function cleanString($string) {
+    $string = parent::cleanString($string);
+    $remove = array(
+      "\t",
+    );
+    // Replace these with spaces.
+    $string = str_ireplace($remove, '', $string);
+    return $string;
+  }
 }
